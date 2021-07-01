fb608c47a48decf8f7a5cc444d7bd64817dee442
fanpengfei1 <fanpengfei@jd.com>
2021-07-01 20:31:16 +0800

    fw

--- mydemo/test_dataframe.py
+++ mydemo/test_dataframe.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-# create df use dict
+# create df use dict, key == col-key , value == col-value
 data = {'state': ['Beijing', 'Shanghai', 'HeBei', 'ShanDong'],
         'year': [2001, 2002, 2003, 2004],
         'pop': [1.5, 1.7, 3.6, 2.4]}
@@ -53,5 +53,24 @@ print("df4:{}".format(df4))
 # 1.2 修改矩阵中某个元素[x,y]的值
 df4['state'].loc['one'] = 'state-yyyy'
 print("df4:{}".format(df4))
+
+
+# add new-col(Series) to DataFrame
+df5 = pd.DataFrame(data, index=['one', 'tow', 'tree', 'four'])
+print('df5:{}'.format(df5))
+
+s1 = pd.Series([1, 2, 3], index=['one', 'tow', 'tree'])
+print("s1:{}".format(s1))
+df5['new col'] = s1
+print("df5:{}".format(df5))
+
+# del col
+del df5['new col']
+print("del df5['new col']:{}".format(df5))
+
+# TODO: add new row to DataFrame: dataframe是按列组织数据的，每一列是一个series， 添加一行相当于给每一列（series）添加一个新元素
+
+
+
 
 
